6127528e8c137f53d2a6324107f470db9b75d26e
DBroomes <15398621+DBroomes@users.noreply.github.com>
2026-06-23 18:52:49 -0400

    Added scripts. Corrected relevant names

---
+++ python1/passingScore.py
@@ -0,0 +1,9 @@
+# 1. Variable: Storing data (a score)
+score = 85
+
+# 2. Operator: Comparing the variable to a value using the #"greater than or equal to" (>=) operator
+# 3. Conditional: Using an "if-else" structure to make a #decision
+if score >= 70:
+    print("Pass")
+else:
+    print("Fail")
---
+++ python2/allowedToDrink.py
@@ -0,0 +1,8 @@
+# Variable
+age = 20
+
+# Operator (<) and Conditional (if-else)
+if age < 21:
+    print("Access denied.")
+else:
+    print("Access granted.")
